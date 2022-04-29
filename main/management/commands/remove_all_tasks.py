ac6dc3ec9d5b6626915f86da37afe3c3ee572a41
jan <janlemcke@live.de>
2022-04-29 09:59:12 +0200

    Management module added.

--- main/management/commands/remove_all_tasks.py
+++ main/management/commands/remove_all_tasks.py
@@ -2,8 +2,14 @@ import background_task.models
 from django.core.management.base import BaseCommand
 from background_task import background
 
+
 class Command(BaseCommand):
     help = 'Set background tasks.'
 
     def handle(self, *args, **kwargs):
-        print("#Tasks:", background_task.models.Task.objects.all().count())
+        number = background_task.models.Task.objects.all().count()
+        print("#Tasks:", number)
+        background_task.models.Task.objects.all().delete()
+        background_task.models.CompletedTask.objects.all().delete()
+        number = background_task.models.Task.objects.all().count()
+        print("#Tasks after delete:", number)

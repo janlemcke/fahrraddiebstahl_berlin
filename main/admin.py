cffff957d15606b98f9bf5c8ffd818d5ab97ad56
jan <janlemcke@live.de>
2022-04-29 11:32:57 +0200

    Reportadmin added.

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from main.models import Report
 
+
 # Register your models here.
-admin.site.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ('createdDay', 'beginDay', 'beginHour', 'endDay', 'endHour')
+
+    filter_horizontal = ()
+    list_filter = ()
+    ordering = ('createdDay',)  # Contain only fields in your `custom-user-model` intended to ordering
+    fieldsets = ()
+
+
+admin.site.register(Report, ReportAdmin)
